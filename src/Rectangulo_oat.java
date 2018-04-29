7c5218ff39c1c681824bdba6cf396db4917abe4a
oscar arjona <oscar@DESKTOP-373J9G9>
2018-04-29 22:42:05 +0200

    proyecto comentado

--- src/Rectangulo_oat.java
+++ src/Rectangulo_oat.java
@@ -1,19 +1,32 @@
-
+/**
+ * @ author oscar arjona tena
+ * @ version 1.1
+ */
 public class Rectangulo_oat extends FiguraGeometrica_oat {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * @param l1 medida del lado mayor
+	 * @param l2 medida del lado menor
+	 */
+
 	public Rectangulo_oat(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+/**
+ * metodo para calcular el area del rectangulo a partir de sus lados
+ * @return area
+ */	
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * metodo para calcular el perimetro a partir de sus lados
+	 * @return perimetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
